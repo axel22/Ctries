336846b09f21d4a5a34b87d9f1a4a3ce57ea024d
Aleksandar Prokopec <prokopec@lamppc18.epfl.ch>
2011-05-05 10:16:52 +0200

    Adding memory consumption benches.

--- src/bench/scala/ctries/MemoryOccupancyTest.scala
+++ src/bench/scala/ctries/MemoryOccupancyTest.scala
@@ -67,3 +67,59 @@ object AfterDeleteCtrie2 extends Benchmark {
 }
 
 
+object MemoryCHM extends Benchmark {
+  import java.util.concurrent.ConcurrentHashMap
+  
+  def run() {
+    val chm = new ConcurrentHashMap[Elem, Elem]
+    val e = elems
+    
+    for (i <- 0 until sz) chm.put(e(i), e(i))
+    
+    Runtime.getRuntime.gc()
+    while (true) {}
+  }
+}
+
+
+object MemorySkipList extends Benchmark {
+  import java.util.concurrent.ConcurrentSkipListMap
+  
+  def run() {
+    val skiplist = new ConcurrentSkipListMap[Elem, Elem]
+    val e = elems
+    
+    for (i <- 0 until sz) skiplist.put(e(i), e(i))
+    
+    Runtime.getRuntime.gc()
+    while (true) {}
+  }
+}
+
+
+object MemoryCtrie extends Benchmark {
+  def run() {
+    val ctrie = new ctries.ConcurrentTrie[Elem, Elem]
+    val e = elems
+    
+    for (i <- 0 until sz) ctrie.insert(e(i), e(i))
+    
+    Runtime.getRuntime.gc()
+    while (true) {}
+  }
+}
+
+
+object MemoryCtrie2 extends Benchmark {
+  def run() {
+    val ctrie = new ctries2.ConcurrentTrie[Elem, Elem]
+    val e = elems
+    
+    for (i <- 0 until sz) ctrie.insert(e(i), e(i))
+    
+    Runtime.getRuntime.gc()
+    while (true) {}
+  }
+}
+
+
