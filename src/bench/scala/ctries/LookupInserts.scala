3a5eba239272d8f1ba96c64ffc5327360e45cfb8
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-06-06 21:58:45 +0200

    Adding the skiplist map bench for the lookup/insert.

--- src/bench/scala/ctries/LookupInserts.scala
+++ src/bench/scala/ctries/LookupInserts.scala
@@ -43,3 +43,40 @@ object LookupInsertsCHM extends Benchmark {
   }
 }
 
+
+object LookupInsertsSkipList extends Benchmark {
+  import java.util.concurrent.ConcurrentSkipListMap
+  
+  def run() {
+    val chm = new ConcurrentSkipListMap[Elem, Elem]
+    val p = par.get
+    val step = sz / p
+    
+    val ins = for (i <- 0 until p) yield new Worker(chm, i, step)
+    
+    for (i <- ins) i.start()
+    for (i <- ins) i.join()
+  }
+  
+  class Worker(chm: ConcurrentSkipListMap[Elem, Elem], n: Int, step: Int) extends Thread {
+    override def run() {
+      val ratio = lookupratio.get
+      var i = n * step
+      val until = (n + 1) * step
+      val e = elems
+      while (i < until) {
+        // do an insert
+        chm.put(e(i), e(i))
+        i += 1
+        
+        // do some lookups
+        var j = 0
+        while (j < ratio) {
+          chm.get(e(math.abs(j * 0x9e3775cd) % i))
+          j += 1
+        }
+      }
+    }
+  }
+}
+
