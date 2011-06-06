b47fc43655150d7dabb3b723014419cc47c4cd79
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-06-07 00:45:34 +0200

    Added fixed size lookup/insert bench for chm.

---
+++ src/bench/scala/ctries/FixedLookupInserts.scala
@@ -0,0 +1,45 @@
+package ctries
+
+
+
+import Global._
+import scala.testing.Benchmark
+
+
+
+object FixedLookupInsertsCHM extends Benchmark {
+  import java.util.concurrent.ConcurrentHashMap
+  val chm = new ConcurrentHashMap[Elem, Elem]
+  
+  def run() {
+    val p = par.get
+    val step = sz / p
+    
+    val ins = for (i <- 0 until p) yield new Worker(chm, i, step)
+    
+    for (i <- ins) i.start()
+    for (i <- ins) i.join()
+  }
+  
+  class Worker(chm: ConcurrentHashMap[Elem, Elem], n: Int, step: Int) extends Thread {
+    override def run() {
+      val ratio = lookupratio.get
+      var i = n * step
+      val until = (n + 1) * step
+      val e = elems
+      while (i < until) {
+        // do an insert
+        chm.put(e(i), e(i))
+        i += 1
+        
+        // do some lookups
+        var j = 0
+        while (j < ratio) {
+          chm.get(e(math.abs(j * 0x9e3775cd) % i))
+          j += 1
+        }
+      }
+    }
+  }
+}
+
