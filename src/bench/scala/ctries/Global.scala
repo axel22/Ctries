7f7d4b8c87e48df43883d0afa2a80dd8f3d8b0e5
Aleksandar Prokopec <prokopec@wolf.cs.oswego.edu>
2011-07-08 03:48:42 -0400

    Added an update benchmarks mixing all 3 basic ops

--- src/bench/scala/ctries/Global.scala
+++ src/bench/scala/ctries/Global.scala
@@ -23,6 +23,10 @@ case class Elem(i: Int) extends Comparable[Elem] {
 object Global {
   val sz = System.getProperty("sz").toInt
   val par = Option(System.getProperty("par")).map(_.toInt)
+  val lookups = Option(System.getProperty("lookups")).map(_.toInt)
+  val inserts = Option(System.getProperty("inserts")).map(_.toInt)
+  val removes = Option(System.getProperty("removes")).map(_.toInt)
+  val totalops = Option(System.getProperty("totalops")).map(_.toInt)
   val lookupratio = Option(System.getProperty("lookupratio")).map(_.toInt)
   val elems = (for (i <- 0 until (sz * 2)) yield Elem(i)).toArray
 }
