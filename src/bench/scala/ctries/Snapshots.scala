251586842fdeaf30b80ffa888b963cc0e0aeaed4
Aleksandar Prokopec <prokopec@lamppc18.epfl.ch>
2011-07-25 18:24:32 +0200

    Added another bench.

--- src/bench/scala/ctries/Snapshots.scala
+++ src/bench/scala/ctries/Snapshots.scala
@@ -132,6 +132,72 @@ object MultiRemovingCtrie2Snapshot extends Benchmark {
 }
 
 
+object MultiLookingCtrie2 extends Benchmark {
+  import ctries2.ConcurrentTrie
+  
+  val ct = new ConcurrentTrie[Elem, Elem]
+  
+  override def setUp() {
+    for (i <- 0 until sz) ct.update(elems(i), elems(i))
+  }
+  
+  def run() {
+    val p = par.get
+    val step = sz / p
+    
+    val ins = for (i <- 0 until p) yield new Looker(ct, i, step)
+    
+    for (i <- ins) i.start()
+    for (i <- ins) i.join()
+  }
+  
+  class Looker(ct: ConcurrentTrie[Elem, Elem], n: Int, step: Int) extends Thread {
+    override def run() {
+      var i = n * step
+      val until = (n + 1) * step
+      val e = elems
+      
+      while (i < until) {
+        ct.lookup(e(i))
+        i += 1
+      }
+    }
+  }
+}
+
+
+object MultiLookingCtrie2Snapshot extends Benchmark {
+  import ctries2.ConcurrentTrie
+  
+  val ct = new ConcurrentTrie[Elem, Elem]
+  for (i <- 0 until sz) ct.put(elems(i), elems(i))
+  
+  def run() {
+    val p = par.get
+    val step = sz / p
+    val snap = ct.snapshot()
+    
+    val ins = for (i <- 0 until p) yield new Looker(snap, i, step)
+    
+    for (i <- ins) i.start()
+    for (i <- ins) i.join()
+  }
+  
+  class Looker(snap: ConcurrentTrie[Elem, Elem], n: Int, step: Int) extends Thread {
+    override def run() {
+      var i = n * step
+      val until = (n + 1) * step
+      val e = elems
+      
+      while (i < until) {
+        snap.lookup(e(i))
+        i += 1
+      }
+    }
+  }
+}
+
+
 
 
 
